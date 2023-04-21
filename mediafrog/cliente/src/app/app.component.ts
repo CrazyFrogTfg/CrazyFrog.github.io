a7cf33294ccd902c0b050591c8a3e23720275b9e
ireneplanes <100580452+ireneplanes@users.noreply.github.com>
2023-04-21 13:02:05 +0200

    navbar responsive vista movil + fuente
    hay que solucionar que la rana se mueve al cambiar el ancho de pantalla

--- mediafrog/cliente/src/app/app.component.ts
+++ mediafrog/cliente/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 @Component({
   selector: 'app-root',
--- mediafrog/cliente/src/app/componentes/shared/navbar/navbar.component.ts
+++ mediafrog/cliente/src/app/componentes/shared/navbar/navbar.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  isNavbarOpen = false;
+
+  toggleNavbar() {
+    this.isNavbarOpen = !this.isNavbarOpen;
+  }
 }
