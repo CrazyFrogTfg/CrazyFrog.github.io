e17a2637869c246bc2198cda6ea0f5b01fa04155
AaronMirandaAlbaladejo <102032214+AaronMirandaAlbaladejo@users.noreply.github.com>
2023-05-10 19:36:05 +0200

    Pruebas mostrar img Artista en buscador

--- mediafrog/cliente/src/app/componentes/pages/artista/artista.component.ts
+++ mediafrog/cliente/src/app/componentes/pages/artista/artista.component.ts
@@ -5,6 +5,8 @@ import { Album } from '../../../interfaces/album.interface'
 import { Cancion } from '../../../interfaces/cancion.interface'
 import { ArtistasService } from 'src/app/servicios/artistas.service';
 import { Input } from '@angular/core';
+import { Storage, ref, uploadBytes, listAll, getDownloadURL } from '@angular/fire/storage';
+
 
 @Component({
   selector: 'app-artista',
@@ -13,21 +15,54 @@ import { Input } from '@angular/core';
 })
 export class ArtistaComponent {
 @Input() artista:any;
+  imageArtist:string=""
   formulario: FormGroup
+  
 
-  constructor(private artistaService: ArtistasService){
+  constructor(private artistaService: ArtistasService,
+              private storage:Storage){
   this.formulario = new FormGroup({
     nombre: new FormControl(),
     descripcion:new FormControl()
     })
   }
-    ngOnInit(): void{ }
-
-    async onSubmit() {
-      console.log(this.formulario.value)
-      const response = await this.artistaService.addArtista(this.formulario.value)
-      console.log(response)
+  ngOnInit(){
     }
+
+  async onSubmit() {
+    console.log(this.formulario.value)
+    const response = await this.artistaService.addArtista(this.formulario.value)
+    console.log(response)
+  }
+
+  //PRUEBAS PARA PODER MOSTRAR IMAGEN DEL ARTISTA. ME TENGO QUE IR, COMMITEO.
+
+  getimageArtist(artistName:string)
+  {
+    const imagesRef = ref(this.storage, `${artistName}`)
+    listAll(imagesRef)
+    .then(async response =>{
+      console.log(response)
+
+      for(let item of response.items){
+        this.imageArtist = await getDownloadURL(item);
+      }
+    })
+    .catch(error => console.log(error))
+  }
+
+  uploadimageArtist($event:any){
+    const file = $event.target.files[0];
+    console.log("file uploading: " + file)
+    const fileRef = ref(this.storage, `${this.artista.name}/imageArtist`)
+
+    uploadBytes(fileRef, file)
+    .then(response =>{
+    console.log(response);
+    })
+    .catch(error => console.log(error));
+  }
+
 }
 
 
