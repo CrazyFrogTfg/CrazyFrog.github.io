46b33a4986b019c70906d906c68a8a9437e08488
ireneplanes <100580452+ireneplanes@users.noreply.github.com>
2023-05-11 17:04:42 +0200

    intento crud playlist

--- cliente/src/app/componentes/pages/playlist/new-playlist/new-playlist.component.ts
+++ cliente/src/app/componentes/pages/playlist/new-playlist/new-playlist.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UsuariosService } from 'src/app/servicios/usuarios.service';
 
 @Component({
   selector: 'app-new-playlist',
@@ -8,9 +10,41 @@ import { Router } from '@angular/router';
 })
 export class NewPlaylistComponent {
 
-  constructor(private router: Router){}
+  newPlaylist: FormGroup;
+  privada:boolean = true;
+  userInfo:any
+  propietario:string = '';
+
+  async ngOnInit() {
+    this.userInfo = await this.userService.getUserInfo()
+    this.propietario = this.userInfo.email
+    this.newPlaylist.patchValue({
+      propietario: this.propietario
+    });
+  }
+
+  constructor(private router: Router, private userService:UsuariosService){
+    this.newPlaylist = new FormGroup({
+      namePlaylist: new FormControl(),
+      privada: new FormControl(this.privada),
+      propietario: new FormControl('')
+    })
+  }
+
+  // isPrivate(){
+  //   if(this.privada == true){
+  //     this.foro = ''
+  //     console.log(this.foro)
+  //     this.foro = "revisiontalentofuncionarios"
+  //     console.log("El foro seleccionado es:", this.foro)
+  //    }
+  // }
 
   goHome(){
     this.router.navigate(['/home']);
+  }
+
+  onSubmit(){
+    console.log(this.newPlaylist.value);
   }
 }
