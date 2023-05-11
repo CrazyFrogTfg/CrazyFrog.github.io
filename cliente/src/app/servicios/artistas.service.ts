e4e06aeee237a27d759fbacc43a532dbd04442d7
AaronMirandaAlbaladejo <102032214+AaronMirandaAlbaladejo@users.noreply.github.com>
2023-05-11 16:47:10 +0200

    Cambios en Artista
    Añadida funcion delete, crud casi entero. Editada interfaz para tomar el id(necesario para delete), o se podria con nombre, hay que verlo

--- cliente/src/app/servicios/artistas.service.ts
+++ cliente/src/app/servicios/artistas.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, addDoc, collectionData } from '@angular/fire/firestore';
+import { Firestore, collection, addDoc, collectionData, doc, deleteDoc } from '@angular/fire/firestore';
 import { Artista } from '../interfaces/artista.interface';
 import { Album } from '../interfaces/album.interface';
 import { Cancion } from '../interfaces/cancion.interface';
@@ -21,4 +21,11 @@ export class ArtistasService {
     const artistaRef = collection(this.firestore, 'artistas')
     return collectionData(artistaRef, { idField: 'name'}) as Observable<Artista[]>;
   }
+
+  deleteArtista(artista:Artista){
+    const artistaDocRef = doc(this.firestore, `artists/${artista.id}`);
+    return deleteDoc(artistaDocRef)
+  }
+
+  
 }
