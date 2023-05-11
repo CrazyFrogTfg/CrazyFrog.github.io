a58ce4b95a278b40d4a2b3b5ec7b18b0c4c3015b
ireneplanes <100580452+ireneplanes@users.noreply.github.com>
2023-05-11 13:47:54 +0200

    color checkbox y funcionalidad boton x

--- cliente/src/app/componentes/pages/playlist/new-playlist/new-playlist.component.ts
+++ cliente/src/app/componentes/pages/playlist/new-playlist/new-playlist.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-playlist',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class NewPlaylistComponent {
 
+  constructor(private router: Router){}
+
+  goHome(){
+    this.router.navigate(['/home']);
+  }
 }
