8e928a732f5f2b4df32a31647665f10399d25bfa
ireneplanes <100580452+ireneplanes@users.noreply.github.com>
2023-05-16 13:35:30 +0200

    obtenidos uids de todos los usuarios

--- cliente/src/app/componentes/admin/users/users.component.ts
+++ cliente/src/app/componentes/admin/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Firestore, collection, query, where, getDocs } from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { User } from 'src/app/interfaces/user.interface';
 import { UsuariosService } from 'src/app/servicios/usuarios.service';
@@ -13,14 +14,20 @@ export class UsersComponent {
   userInfo:any
   users:User[] = []
   isAdmin:boolean = false
+  uids:Array<string> = []
 
-  constructor(private userService:UsuariosService, private router: Router){}
+  constructor(private userService:UsuariosService, private router: Router, private firestore:Firestore){}
 
   async ngOnInit() {
     this.userInfo = await this.userService.getUserInfo()
       if(this.userInfo.admin) {
         this.isAdmin = true
         this.users = await this.userService.getAllUsers()
+        this.users.forEach(async user => {
+          const q = query(collection(this.firestore, "users"), where("email", "==", user.email))
+          const querySnapshots = await getDocs(q)
+          user.id = querySnapshots.docs[0].id
+        });
       }
   }
 
