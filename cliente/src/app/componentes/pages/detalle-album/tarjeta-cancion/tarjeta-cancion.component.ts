b6233198fef56840e50e4c17353df78b83c8cd79
aaron <5800947@alu.murciaeduca.es>
2023-05-25 17:58:20 +0200

    delete y update song funcional

--- cliente/src/app/componentes/pages/detalle-album/tarjeta-cancion/tarjeta-cancion.component.ts
+++ cliente/src/app/componentes/pages/detalle-album/tarjeta-cancion/tarjeta-cancion.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Playlist } from 'src/app/interfaces/playlist.interface';
 import { DbService } from 'src/app/servicios/db.service';
 import { UsuariosService } from 'src/app/servicios/usuarios.service';
@@ -15,13 +17,23 @@ export class TarjetaCancionComponent {
 
   reproduciendo:string = ""
   userUID:any
+  edit:boolean=false
+  updateSong:FormGroup
+  userInfo:any
+  isAdmin:boolean=false
 
-  constructor(private userService:UsuariosService, private db:DbService){}
-
-  async ngOnInit(){
-    this.userUID = await this.userService.getUID()
-    console.log(this.song)
+  constructor(private userService:UsuariosService, private db:DbService, private router:Router){
+    this.updateSong = new FormGroup({
+      name: new FormControl(),
+    })
   }
+
+async ngOnInit(){
+  this.userInfo = await this.userService.getUserInfo()
+    if(this.userInfo.admin) this.isAdmin = true
+  this.userUID = await this.userService.getUID()
+  console.log(this.song)
+}
 
 reproducir(song:string){
   this.reproduciendo = song
@@ -30,6 +42,26 @@ reproducir(song:string){
 
 addSongToPlaylist(playlist:Playlist){
   this.db.addSongToPlaylist(playlist, this.song)
+}
+
+toggleEdit(){
+  this.edit = !this.edit
+}
+
+async onSubmit()
+{
+  if(this.updateSong.value)
+    {
+      console.log("Controlar error de año, que puede ser mayor a la fecha actual")
+      await this.db.updateSong(this.updateSong.value, this.song);
+      this.router.navigate(['/home'])
+    }
+}
+
+deleteSong()
+{
+  this.db.deleteSong(this.song.id)
+  this.router.navigate(['/buscador'])
 }
 
 }
--- cliente/src/app/interfaces/song.interface.ts
+++ cliente/src/app/interfaces/song.interface.ts
@@ -1,5 +1,5 @@
 export interface Song {
-  id?:string,
+  id:string,
   name:string,
   order:number,
   lyrics:string,
