1e8dff70bfcae5b6ec2bc155752f7eb0868399ad
ireneplanes <100580452+ireneplanes@users.noreply.github.com>
2023-06-10 13:40:47 +0200

    aria-label every button

--- cliente/src/app/componentes/pages/detalle-artista/tarjeta-album/tarjeta-album.component.ts
+++ cliente/src/app/componentes/pages/detalle-artista/tarjeta-album/tarjeta-album.component.ts
@@ -3,8 +3,6 @@ import { Router } from '@angular/router';
 import { DbService } from 'src/app/servicios/db.service';
 import { FormControl, FormGroup } from '@angular/forms';
 import { UsuariosService } from 'src/app/servicios/usuarios.service';
-import { Album } from 'src/app/interfaces/album.interface';
-
 
 @Component({
   selector: 'app-tarjeta-album',
@@ -12,70 +10,60 @@ import { Album } from 'src/app/interfaces/album.interface';
   styleUrls: ['./tarjeta-album.component.css']
 })
 export class TarjetaAlbumComponent {
-@Input() album:any
-@Input() artista:any
+  @Input() album:any
+  @Input() artista:any
 
-edit:boolean=false
-updateAlbum:FormGroup
-myEvent:any
-isAdmin:boolean = false
-userInfo:any
-albumFav:any
+  updateAlbum:FormGroup
+  myEvent:any
+  isAdmin:boolean = false
+  userInfo:any
+  albumFav:any
 
 
-constructor(private db:DbService, private router:Router, private userService:UsuariosService){
-  this.updateAlbum = new FormGroup({
-    id: new FormControl(),
-    name: new FormControl(),
-    year: new FormControl(),
-  })
-}
-
-async ngOnInit()
-{
-  this.userInfo = await this.userService.getUserInfo()
-  if(this.userInfo.admin) this.isAdmin = true
-  setTimeout( () => undefined, 2000)
-}
-
-onSubmit(){
-
-}
-
-toggleEdit(){
-  this.edit = !this.edit
-}
-
-setMyEvent($event:any){
-  this.myEvent = $event
-}
-
-truncateTitle(title: string, maxLength: number): string {
-  if (title.length <= maxLength) {
-    return title;
-  } else {
-    let truncatedTitle = title.slice(0, maxLength);
-    if (truncatedTitle.charAt(maxLength - 1) === ' ') {
-      truncatedTitle = truncatedTitle.slice(0, maxLength - 1);
-    }
-    return truncatedTitle + '...';
+  constructor(private db:DbService, private router:Router, private userService:UsuariosService){
+    this.updateAlbum = new FormGroup({
+      id: new FormControl(),
+      name: new FormControl(),
+      year: new FormControl(),
+    })
   }
-}
 
-async goToDetails(album: any) {
-  this.router.navigate(['/album'], { queryParams: { idArtist: this.artista, idAlbum: album.id } });
-}
+  async ngOnInit()
+  {
+    this.userInfo = await this.userService.getUserInfo()
+    if(this.userInfo.admin) this.isAdmin = true
+    setTimeout( () => undefined, 2000)
+  }
 
-setAlbumFav(){
-  this.db.setAlbumFav(this.album.id)
-}
+  setMyEvent($event:any){
+    this.myEvent = $event
+  }
 
-delAlbumFav(){
-  this.db.delAlbumFav(this.album.id)
-}
+  truncateTitle(title: string, maxLength: number): string {
+    if (title.length <= maxLength) {
+      return title;
+    } else {
+      let truncatedTitle = title.slice(0, maxLength);
+      if (truncatedTitle.charAt(maxLength - 1) === ' ') {
+        truncatedTitle = truncatedTitle.slice(0, maxLength - 1);
+      }
+      return truncatedTitle + '...';
+    }
+  }
 
-isAlbumFav(){
-  return this.db.isAlbumFav(this.album.id)
-}
+  async goToDetails(album: any) {
+    this.router.navigate(['/album'], { queryParams: { idArtist: this.artista, idAlbum: album.id } });
+  }
 
+  setAlbumFav(){
+    this.db.setAlbumFav(this.album.id)
+  }
+
+  delAlbumFav(){
+    this.db.delAlbumFav(this.album.id)
+  }
+
+  isAlbumFav(){
+    return this.db.isAlbumFav(this.album.id)
+  }
 }
